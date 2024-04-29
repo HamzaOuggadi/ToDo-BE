a69809559ddee9d856f7f24e41b73506a01af1d0
Hamza Ouggadi <45943796+HamzaOuggadi@users.noreply.github.com>
2024-04-29 13:04:33 +0100

    First Commit

---
+++ src/main/java/net/hamzaouggadi/todobe/ToDoBeApplication.java
@@ -0,0 +1,13 @@
+package net.hamzaouggadi.todobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/hamzaouggadi/todobe/ToDoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.hamzaouggadi.todobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
